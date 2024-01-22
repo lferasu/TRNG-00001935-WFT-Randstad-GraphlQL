86f17ef0450de4a713697f74c644270eec25cdd8
Surafel Teshome <lferasu@yahoo.com>
2024-01-22 14:32:02 -0500

    bunch of installs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { buildSchema } from "graphql";
 import { createHandler } from "graphql-http/lib/use/express";
 import ruru from 'ruru/server'
+import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader'
+import {loadSchema} from  '@graphql-tools/load'
 
 
 
