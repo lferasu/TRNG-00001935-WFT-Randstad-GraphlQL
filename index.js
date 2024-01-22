4c49f4bbe96073b629d56bbd563a5c80bd3f934b
Surafel Teshome <lferasu@yahoo.com>
2024-01-22 14:27:11 -0500

    boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-import { graphql, buildSchema } from "graphql";
+import express from "express";
+import { buildSchema } from "graphql";
+import { createHandler } from "graphql-http/lib/use/express";
+import ruru from 'ruru/server'
+
+
+
+
+
+
+
+// construct a schema
+
+// provide a resolver
+
+// run graphQL
+
